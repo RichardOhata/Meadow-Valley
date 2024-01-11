63d7de92f8b736f5c1e550bc3ab98badd99fda7b
RichardOhata <richardohata@gmail.com>
2024-01-11 00:16:09 -0800

    Added fade in effect in title screen:

--- Assets/ButtonLogic.cs
+++ Assets/ButtonLogic.cs
@@ -12,7 +12,28 @@ public class ButtonLogic : MonoBehaviour
     [SerializeField] private Button startButton;
     [SerializeField] private Button settingsButton;
     [SerializeField] private Button quitButton;
+  
 
+    private void Start()
+    {
+        StartCoroutine(FadeInButton(startButton, 0f));
+        StartCoroutine(FadeInButton(settingsButton, 0.5f));
+        StartCoroutine(FadeInButton(quitButton, 1f));
+
+    }
+    private IEnumerator FadeInButton(Button button, float delay)
+    {
+
+        yield return new WaitForSeconds(delay);
+        Debug.Log(button.gameObject.name);
+        CanvasGroup canvasGroup = button.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+        while (canvasGroup.alpha <= 1)
+        {
+            canvasGroup.alpha += Time.deltaTime;
+            yield return null;
+        }
+    }
 
     public void StartGame()
     {
